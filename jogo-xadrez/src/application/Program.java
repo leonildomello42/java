157a9db2fe8a40a77efeee121e20a01acae102d1
leonildomello42 <leonildo.mello@gmail.com>
2022-12-26 21:47:08 -0300

    BoardException and defensive programming

--- jogo-xadrez/src/application/Program.java
+++ jogo-xadrez/src/application/Program.java
@@ -4,11 +4,13 @@ import boardgame.Board;
 import boardgame.Position;
 import chess.ChessMatch;
 
+import static application.UI.printBoard;
+
 public class Program {
 
     public static void main(String[] args) {
 
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+        printBoard(chessMatch.getPieces());
     }
 }
