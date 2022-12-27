84393b66d65b8ed77e4969d14f6f35ce21b6c4c9
leonildomello42 <leonildo.mello@gmail.com>
2022-12-27 16:54:46 -0300

    Little improvement in board printing

--- jogo-xadrez/src/application/Program.java
+++ jogo-xadrez/src/application/Program.java
@@ -10,7 +10,6 @@ public class Program {
 
     // java application/Program
     public static void main(String[] args) {
-
         ChessMatch chessMatch = new ChessMatch();
         printBoard(chessMatch.getPieces());
     }
