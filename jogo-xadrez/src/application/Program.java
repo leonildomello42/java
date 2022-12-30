b1841f67ce5b4df9982226eec013fe141e46d80b
leonildomello42 <leonildo.mello@gmail.com>
2022-12-30 20:01:37 -0300

    Moving pieces

--- jogo-xadrez/src/application/Program.java
+++ jogo-xadrez/src/application/Program.java
@@ -3,14 +3,36 @@ package application;
 import boardgame.Board;
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 import static application.UI.printBoard;
 
 public class Program {
 
     // java application/Program
+
     public static void main(String[] args) {
+
+//        ChessMatch chessMatch = new ChessMatch();
+//        printBoard(chessMatch.getPieces());
+
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        printBoard(chessMatch.getPieces());
+
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
